391226c925886cca12a5b7ad690810b6f5dec145
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2015-04-23 15:19:58 -0400

    kk

---
+++ American Toad/test/AmericanToadGameTest/AmericanToadTest.java
@@ -0,0 +1,37 @@
+package AmericanToadGameTest;
+
+import junit.framework.TestCase;
+
+public class AmericanToadTest extends TestCase {
+	
+	
+
+	public void testHasWon() {
+		fail("Not yet implemented");
+	}
+
+	public void testInitialize() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetBaseRank() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetBaseRank() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetPreferredSize() {
+		fail("Not yet implemented");
+	}
+
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
